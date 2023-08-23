2fafeba071108ddec22a952cd8e7709f185f7c1d
John Hopper <jhopper@specterops.io>
2023-08-22 21:23:22 -0700

    checkpoint

--- packages/go/dawgs/drivers/pg/query/sql.go
+++ packages/go/dawgs/drivers/pg/query/sql.go
@@ -1,9 +1,26 @@
+// Copyright 2023 Specter Ops, Inc.
+//
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package query
 
 import (
 	"embed"
 	"fmt"
 	"path"
+	"strings"
 )
 
 var (
@@ -11,14 +28,30 @@ var (
 	queryFS embed.FS
 )
 
-func readFile(name string) string {
-	content, err := queryFS.ReadFile(name)
+func stripSQLComments(multiLineContent string) string {
+	builder := strings.Builder{}
 
-	if err != nil {
-		panic(fmt.Sprintf("Unable to find embedded query file %s: %v", name, err))
+	for _, line := range strings.Split(multiLineContent, "\n") {
+		trimmedLine := strings.TrimSpace(line)
+
+		// Strip empty and SQL comment lines
+		if len(trimmedLine) == 0 || strings.HasPrefix(trimmedLine, "--") {
+			continue
+		}
+
+		builder.WriteString(trimmedLine)
+		builder.WriteString("\n")
 	}
 
-	return string(content)
+	return builder.String()
+}
+
+func readFile(name string) string {
+	if content, err := queryFS.ReadFile(name); err != nil {
+		panic(fmt.Sprintf("Unable to find embedded query file %s: %v", name, err))
+	} else {
+		return stripSQLComments(string(content))
+	}
 }
 
 func loadSQL(name string) string {
